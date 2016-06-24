2600f76c06d430452e8810a6794c8251ee22a307
dollar007 <vlad.karapetyan.1988@mail.ru>
2016-06-24 14:48:01 +0400

    register_site_test (added op_host) update for version 2.4.4

--- protocolTests/Register_site_test.php
+++ protocolTests/Register_site_test.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../src/oxdrp/Autoload.php';
 session_start();
 session_destroy();
 $register_site = new Register_site();
-
+$register_site->setRequestOpHost(Oxd_RP_config::$op_host);
 $register_site->setRequestAcrValues(Oxd_RP_config::$acr_values);
 $register_site->setRequestAuthorizationRedirectUri(Oxd_RP_config::$authorization_redirect_uri);
 $register_site->setRequestRedirectUris(Oxd_RP_config::$redirect_uris);
