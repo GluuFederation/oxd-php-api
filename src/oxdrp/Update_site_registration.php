d81a513555dc2058812d1b2ccf398a08b1c0ad75
dollar007 <vlad.karapetyan.1988@mail.ru>
2016-08-15 18:15:57 +0400

    removing redirect_uris from api , last task

--- src/oxdrp/Update_site_registration.php
+++ src/oxdrp/Update_site_registration.php
@@ -55,8 +55,7 @@
 namespace oxdrp;
 use oxdrp\Client_OXD_RP;
 
-class Update_site_registration extends Client_OXD_RP
-{
+class Update_site_registration extends Client_OXD_RP{
     /**
      * @var string $request_oxd_id                          This parameter you must get after registration site in gluu-server
      */
@@ -73,10 +72,6 @@ class Update_site_registration extends Client_OXD_RP
      * @var string $request_application_type                web or mobile
      */
     private $request_application_type = null;
-    /**
-     * @var array $request_redirect_uris                    Site redirect uris after login or logout
-     */
-    private $request_redirect_uris = null;
     /**
      * @var array $request_acr_values                       Gluu login acr type, can be basic, duo, u2f, gplus and etc.
      */
@@ -306,23 +301,6 @@ class Update_site_registration extends Client_OXD_RP
     /**
      * @return array
      */
-    public function getRequestRedirectUris()
-    {
-        return $this->request_redirect_uris;
-    }
-
-    /**
-     * @param array $request_redirect_uris
-     * @return void
-     */
-    public function setRequestRedirectUris($request_redirect_uris)
-    {
-        $this->request_redirect_uris = $request_redirect_uris;
-    }
-
-    /**
-     * @return array
-     */
     public function getRequestAcrValues()
     {
         return $this->request_acr_values;
@@ -407,7 +385,6 @@ class Update_site_registration extends Client_OXD_RP
             "oxd_id" => $this->getRequestOxdId(),
             "post_logout_redirect_uri" => $this->getRequestLogoutRedirectUri(),
             "application_type" => $this->getRequestApplicationType(),
-            "redirect_uris" => $this->getRequestRedirectUris(),
             "acr_values" => $this->getRequestAcrValues(),
             "scope" => $this->getRequestScope(),
             "client_jwks_uri" => $this->getRequestClientJwksUri(),
