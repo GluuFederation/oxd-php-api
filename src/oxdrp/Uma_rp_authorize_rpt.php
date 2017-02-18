004bdfef65bfd47ebc8506a3bbb2933686d3a67b
karapetyan88 <mr.karapetyan88@gmail.com>
2017-02-18 17:19:40 +0400

    fixing documentation link

--- src/oxdrp/Uma_rp_authorize_rpt.php
+++ src/oxdrp/Uma_rp_authorize_rpt.php
@@ -15,7 +15,7 @@
 	 *
 	 * @author    Gluu Inc.          : <https://gluu.org>
 	 * @link      Oxd site           : <https://oxd.gluu.org>
-	 * @link      Documentation      : <https://oxd.gluu.org/docs/3.0.0/libraries/php/>
+	 * @link      Documentation      : <https://oxd.gluu.org/docs/libraries/php/>
 	 * @director  Mike Schwartz      : <mike@gluu.org>
 	 * @support   Support page       : <support@gluu.org>
 	 * @developer Volodya Karapetyan : <https://github.com/karapetyan88> <mr.karapetyan88@gmail.com>
